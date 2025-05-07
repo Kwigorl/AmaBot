62734477e4f735f9d068c253934be8ef8efae3e8
Kwigorl <143103853+Kwigorl@users.noreply.github.com>
2025-05-07 23:55:31 +0200

    Add files via upload

---
+++ post_survey.py
@@ -0,0 +1,27 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+ROLE_ID = int(os.getenv("ROLE_ID"))
+GUILD_ID = int(os.getenv("GUILD_ID"))client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Bot connecté en tant que {client.user}')
+
+    # Récupère le canal où envoyer le message
+    guild = client.get_guild(int(GUILD_ID))
+    channel = guild.get_channel(int(CHANNEL_ID))
+
+    # Envoie un message avec les réactions
+    message = await channel.send(f"<@&{ROLE_ID}>Tu viens jouer ce soir ? Votez avec :\n👍 Oui\n👎 Non\n❓ Peut-être")
+
+    # Ajouter les réactions
+    await message.add_reaction('👍')
+    await message.add_reaction('👎')
+    await message.add_reaction('❓')
+
+client.run(TOKEN)
